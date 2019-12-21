aa50e748b6e4372124b29a3847674b6202cfbb00
ekuone <56842648+ekuone@users.noreply.github.com>
2019-12-21 15:20:56 +0600

    Update turn_off.py

--- turn_off.py
+++ turn_off.py
@@ -8,10 +8,12 @@ class Turn_off:
          self.time_limit = time_limit
          eku = Timer(time_limit, self.force_exit)
          eku.start()
-
+# вырубает экран. комп остается включенныймб только дисплей вырубает!
     def force_exit(self):
         my_display = 0xF170
         win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, my_display, 2)
-
+       
+#если хочкшь вырубить сам комп то вместо (def force_exit(self):.....) поставь     """def force_exit(self): os.system("shutdown /s /t 1")"""         
+     
     """def force_exit(self):
         os.system("shutdown /s /t 1")"""
